c2c895859db62578176d297b954caf1172cba7d2
Akash Shridharan <akashshridharan2802@gmail.com>
2023-07-12 12:52:56 +0530

    billboards entity(admin) created

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -4,15 +4,6 @@ import { StoreModal } from "@/components/modals/store-modal";
 import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if(!isMounted){
-        return null;
-    }
 
     return (
         <>
